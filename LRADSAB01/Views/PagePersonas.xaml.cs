172380fb479adbed44fc46a9d74546d6a2f5f9dd
ricardo.lagos <ricardo.lagoshn@gmail.com>
2023-07-01 10:45:38 -0600

    Cambios al 01/07/2023

--- LRADSAB01/Views/PagePersonas.xaml.cs
+++ LRADSAB01/Views/PagePersonas.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Media;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +13,33 @@ namespace LRADSAB01.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PagePersonas : ContentPage
     {
+        
+        // Variable que guardara la foto 
+        Plugin.Media.Abstractions.MediaFile photo = null;
+
         public PagePersonas()
         {
             InitializeComponent();
         }
+
+
+        public byte[] GetImageToBytes() 
+        {
+            if (photo != null)
+            {
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    Stream stream = photo.GetStream();
+                    stream.CopyTo(memory);
+                    byte[] bytes = memory.ToArray();
+
+                    return bytes;
+                }
+            }
+
+            return null;
+        }
+
 
         private async void btnproceso_Clicked(object sender, EventArgs e)
         {
@@ -25,7 +49,7 @@ namespace LRADSAB01.Views
                 Apellidos = Apellidos.Text,
                 FechaNac = FechaNac.Date,
                 telefono = Telefono.Text,
-                foto = null
+                foto = GetImageToBytes()
             };
 
             if (await App.Database.AddPersona(person) > 0)
@@ -39,9 +63,17 @@ namespace LRADSAB01.Views
 
         }
 
-        private void btnfoto_Clicked(object sender, EventArgs e)
+        private async void btnfoto_Clicked(object sender, EventArgs e)
         {
+            photo = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions 
+            {
+                Directory = "APP",Name = "foto.jpg", SaveToAlbum= true
+            });
 
+            if (photo != null)
+            {
+                foto.Source = ImageSource.FromStream(()=> { return photo.GetStream(); });
+            }
         }
     }
 }
